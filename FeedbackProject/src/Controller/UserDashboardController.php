52be177c1b6ceb18eb99753b3c31a4834ae76e8d
RaulAM <raulartilesm@gmail.com>
2024-11-12 16:13:31 +0000

    Update

--- FeedbackProject/src/Controller/UserDashboardController.php
+++ FeedbackProject/src/Controller/UserDashboardController.php
@@ -5,13 +5,80 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserDashboardController extends AbstractController
 {
-    #[Route('/user/dashboard', name: 'app_user_dashboard')]
+    private EntityManagerInterface $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
-        return $this->render('user_dashboard/index.html.twig', [
+        $user = $this->getUser();
+        if (!$user instanceof User) {
+            throw $this->createNotFoundException('User not found');
+        }
+        $id = $user->getId();
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserDashboardController',
+            'id' => $id,
+        ]);
+    }
+
+    #[Route('/user/updateToData', name: 'app_user_update', methods: ['POST', 'PUT'])]
+    public function updateData(Request $request): Response
+    {
+        return $this->redirect('app_user_dashboard');
+        $user = $this->getUser();
+
+        if (!$user instanceof User) {
+            throw $this->createNotFoundException('User not found');
+        }
+        // if ($request->get('_method') === 'POST')
+        // {
+        $user->setName($request->request->get('name'));
+        $user->setEmail($request->request->get('email'));
+        $user->setPhoneNumber($request->request->get('phoneNumber'));
+        $user->setCompany($request->request->get('company'));
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('app_user_dashboard');
+        // } 
+        // else
+        // {
+        //     return $this->render('user/updateDataFailure.html.twig', [
+        //         'controller_name' => 'UserupdateDataController',
+        //     ]);            
+        // }
+    }
+
+    #[Route('/user/updateData', name: 'app_user_get_form')]
+    public function updateDataShow(): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof User) {
+            throw $this->createNotFoundException('User not found');
+        }
+        $id = $user->getId();
+        return $this->render('user/updateData.html.twig', [
+            'controller_name' => 'UserupdateDataController',
+            'id' => $id,
+        ]);
+    }
+
+
+    #[Route('/user/dashboard', name: 'app_user_dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('user/dashboard.html.twig', [
             'controller_name' => 'UserDashboardController',
         ]);
     }

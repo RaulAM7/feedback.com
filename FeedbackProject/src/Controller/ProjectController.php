970be733fa7716a681a42bded537679d75211e01
RaulAM <raulartilesm@gmail.com>
2024-11-13 14:49:38 +0000

    Update: start to build MVC /project Controller branch of Views

---
+++ FeedbackProject/src/Controller/ProjectController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Project;
+
+
+
+class ProjectController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    // Ruta publica placeholder
+    #[Route('/project', name: 'app_project')]
+    public function index(): Response
+    {
+        return $this->render('project/index.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+}

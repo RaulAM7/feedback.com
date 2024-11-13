99c005dfbb1a401462801ec45e58b433e5a8f44c
RaulAM <raulartilesm@gmail.com>
2024-11-13 15:10:53 +0000

    Update: PROJECT Create FORM CRUD built

--- FeedbackProject/src/Controller/ProjectController.php
+++ FeedbackProject/src/Controller/ProjectController.php
@@ -8,8 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Project;
-
-
+use App\Form\ProjectCreateForm;
 
 class ProjectController extends AbstractController
 {
@@ -25,6 +24,25 @@ class ProjectController extends AbstractController
     {
         return $this->render('project/index.html.twig', [
             'controller_name' => 'ProjectController',
+        ]);
+    }
+
+    #[Route('/project/create', name: 'app_project_create')]
+
+    public function createProject(Request $request, EntityManagerInterface $entityManagerInterface): Response
+    {
+        $project = new Project();
+        $form = $this->createForm(ProjectCreateForm::class,
+         $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->entityManager;
+            $em->persist($project);
+            $em->flush();
+        }
+        return $this->render('project/projectCreate.html.twig', parameters: [
+            'projectCreateForm' => $form->createView(),
         ]);
     }
 }
--- FeedbackProject/src/Controller/RegistrationController.php
+++ FeedbackProject/src/Controller/RegistrationController.php
@@ -19,7 +19,8 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createForm(RegistrationFormType::class,
+         $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {

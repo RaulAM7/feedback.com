a54917b8a97fa3599ba65d626b593732fc9c45f1
RaulAM <raulartilesm@gmail.com>
2024-11-13 14:55:54 +0000

    Update: start to build MVC /posts Controller branch of Views

--- FeedbackProject/src/Controller/PostController.php
+++ FeedbackProject/src/Controller/PostController.php
@@ -7,24 +7,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Project;
+use App\Entity\Post;
 
-
-
-class ProjectController extends AbstractController
+class PostController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
     }
-
-    // Ruta publica PROJECT VIEW BRANCH INDEX placeholder
-    #[Route('/project', name: 'app_project')]
+    
+    // Ruta publica POST VIEW BRANCH INDEX placeholder
+    #[Route('/posts', name: 'app_posts')]
     public function index(): Response
     {
-        return $this->render('project/index.html.twig', [
-            'controller_name' => 'ProjectController',
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
         ]);
     }
 }

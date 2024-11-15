817acf42454d0bd8f108450f15d2c72a27709523
RaulAM <raulartilesm@gmail.com>
2024-11-15 10:57:25 +0000

    Update: HOME Landing && REGISTER MOCKUPS - IMPLEMENTED IN TEST VERSION AT MVC

--- FeedbackProject/src/Controller/RegistrationController.php
+++ FeedbackProject/src/Controller/RegistrationController.php
@@ -47,4 +47,39 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
+
+    #[Route('/register-test', name: 'app_register-test')]
+    public function registerTest(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(
+            RegistrationFormType::class,
+            $user
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
+        }
+
+        return $this->render('registration/register-test.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
 }

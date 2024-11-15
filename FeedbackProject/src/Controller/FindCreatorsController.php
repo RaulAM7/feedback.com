314ccf825d349b05273e4349e2c2374059aa18a9
RaulAM <raulartilesm@gmail.com>
2024-11-15 12:45:20 +0000

    Find creators View MOCKUP IMPLEMENTED AT MVC

--- FeedbackProject/src/Controller/FindCreatorsController.php
+++ FeedbackProject/src/Controller/FindCreatorsController.php
@@ -8,10 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FindCreatorsController extends AbstractController
 {
-    #[Route('/find/creators', name: 'app_find_creators')]
+    #[Route('/find-creators', name: 'app_find_creators')]
     public function index(): Response
     {
-        return $this->render('find_creators/index.html.twig', [
+        return $this->render('find_creators/findCreators-test1.html.twig', [
             'controller_name' => 'FindCreatorsController',
         ]);
     }
